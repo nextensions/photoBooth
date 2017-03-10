202819ef5b3fabebdaa3f8cf4fcd596a5d44d4f9
pangpond <pondzz@gmail.com>
2017-03-11 00:46:32 +0700

    add realtime clockin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 import 'font-awesome/css/font-awesome.min.css'
 import App from './App'
+import RealtimePage from './Realtime'
 import './index.css'
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="realtime" component={RealtimePage} />
+  </Router>
+), document.getElementById('root'))
