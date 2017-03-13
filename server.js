6950340d56769a6d0e08c6430da888a71f1c405d
Suvichan Prasongphonchai <aod@nextgensoft.co.th>
2017-03-13 14:40:06 +0700

    fixbug

--- server.js
+++
@@ -1,8 +0,0 @@
-var server = require('pushstate-server');
-require('./server/index.js')
-server.start({
-    port: 9000,
-    directory: './build'
-}, function() {
-    console.log("Server start on : http://photobooth.local:9000")
-})
